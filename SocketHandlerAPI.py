ea2e4d03adbbd2e6cc0442a968b8d23aa32bcec0
MarkusOstermayer <markus.ostermayer@ymail.com>
2017-09-18 21:11:28 +0200

    added some comments

--- SocketHandlerAPI.py
+++ SocketHandlerAPI.py
@@ -1,7 +1,14 @@
-#SocketHandler
+#Pythonimports
 import socket
 import threading
 
+#Selfimports
+#none by now
+
+#---------------------------------------------------
+#Class HandleSocketConnections
+#Handels the clientconnections and recvives the data
+#---------------------------------------------------
 class HandleSocketConnections(threading.Thread):
 	def __init__(self,clientsocket,adresse):
 		threading.Thread.__init__(self)
@@ -9,22 +16,28 @@ class HandleSocketConnections(threading.Thread):
 		self.adresse=adresse
 
 	def run(self):
+		#The Handler gets called with the clientsocket and the clientadresse
 		HandleSocketConnections.Handler(self.clientsocket,self.adresse)
 	
 	@staticmethod
 	def Handler(clientsocket,adresse):
 		while True:
+				#The recived data gets stored in the varable data, the maximum lenth is 1024 characters
 				daten = clientsocket.recv(1024)
+				#If the Lenth is 1 or under, the client has disconnected
 				if (len(daten)>1):
 					PrintHandler = Printhandler(daten)
 					PrintHandler.start()
 				else:
+					#If the < than 1, than the client has disconnected and we can close the thread
+					#because ist is not longer needed
 					print("Client %s disconnected"%(str(adresse)))
 					#conClients.remove(str(adresse))
 					break
 
-
-
+#------------------------------------------------------------------------------------
+#Waits for incomming conenctions and than starts a new HandleSocketConnection-Thread
+#------------------------------------------------------------------------------------
 class WaitOnConnection(threading.Thread):
 	def __init__(self,sock):
 		threading.Thread.__init__(self)
@@ -39,6 +52,8 @@ class WaitOnConnection(threading.Thread):
 				clientsocket,adresse = sock.accept()
 				print("Client %s connected to the NCC"%(str(adresse)))
 				#conClients.append(str(adresse))
+				#If a client connects, an new thread gets created and the data is 
+				#being processed in this new thread
 				Clienthandler = HandleSocketConnections(clientsocket,adresse)
 				Clienthandler.start()
 
