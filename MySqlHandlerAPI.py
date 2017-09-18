814018ad02c0df1cf4cb7895b0b6b523457496fc
MarkusOstermayer <markus.ostermayer@ymail.com>
2017-09-18 21:11:14 +0200

    Introduced the json format, added a new konfig funktion and added some comments

--- MySqlHandlerAPI.py
+++ MySqlHandlerAPI.py
@@ -1,7 +1,10 @@
-#MySqlHandlerAPI
+#MPythonimports
 import mysql.connector as mysqlconnector
+import json
+import sys
 
-
+#Selfimports
+#none by now
 
 class MySql():
 
@@ -10,22 +13,25 @@ class MySql():
 		config = Konfig.get("MySql.config")
 		#Gets a Dataset from the MySql
 
-
+	#Work in progress
 	#def InsertDataset():
 		#Addes a new connected client to the Database
 
+	#Work in progress
 	#def RemoveDataset():
 		#Removes a Client from the Database
 
+	#Here, the mysql objekt gets created with the login data from the configfile
 	def CreateObjekt():
 		#Created the MySqlObjekt
 		config = Konfig.get("MySql.cofig")
 		mysqlobjekt = mysqlconnector.connect(
-			config[config.index("Datenbank Username: ")+1],
-			config[config.index("Datenbank Passwort: ")+1],
-			config[config.index("Datenbank Hostname: ")+1],
-			config[config.index("Datenbank Datenbank: ")+1])
+			config['Database']['Username'], #Username
+			config['Database']['Password'],	#Password
+			config['Database']['Hostname'],	#Host
+			config['Database']['Database'])	#and a database are grabed from an configfile
 
+		print(mysqlobjekt)
 		return mysqlobjekt
 
 	#def DestroyObjekt():
@@ -33,19 +39,35 @@ class MySql():
 
 class Konfig():
 
+	#Here, the configfile is being read and the values are later used to connect to the database
 	@staticmethod
 	def get(Filename="MySql.config"):
-		#Here, the config will be grabbed
-		config=[]
-		file = open(Filename,'r')
-		for line in file:
-			daten = line.split("'")
-			for inhalt in daten:
-				if(len(inhalt)>1):
-					config.append(inhalt)
-					print("Line: %s"%(str(inhalt)))
-		
-		return config
+		file = open(Filename).read()
 
-if __name__ == "__main__":
-	MySql.GetDataset()
+		data = json.loads(file)
+		file.close()
+
+		#print(data) #Debugpurposes
+		return data
+
+
+	#If the file is not there jet, it will be created
+	@staticmethod
+	def write(Filename="MySql.config"):
+		file = open(Filename,'w')
+
+		jsonstr = {"Database":{"Username":'[put here]',
+							"Password":'[put here]',
+							"Database":'[put here]',
+							"Host":'[put here]'
+							}}
+
+
+		file.write(json.dumps(jsonstr,indent=4,separators = (',',':')))
+		file.close()
+
+
+#This code is just her for debugingpurposes
+#if __name__ == "__main__":
+	#MySql.GetDataset()
+#	Konfig.write()
